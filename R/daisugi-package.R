b39eae8fac6f3fd069f4174f89038bf1e6a95685
frank hull <frankiethull@gmail.com>
2026-06-11 22:38:17 -0600

    new imports, exports, inc version

--- R/daisugi-package.R
+++ R/daisugi-package.R
@@ -2,11 +2,18 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom dials new_quant_param
 #' @importFrom evtree evtree
 #' @importFrom evtree evtree.control
+#' @importFrom parsnip new_model_spec
+#' @importFrom parsnip set_encoding
+#' @importFrom parsnip set_fit
+#' @importFrom parsnip set_model_arg
+#' @importFrom parsnip set_pred
 #' @importFrom partykit cforest
 #' @importFrom reticulate import
 #' @importFrom reticulate py_require
+#' @importFrom rlang enquo
 #' @importFrom rpart rpart
 #' @importFrom treeClust rpart.predict.leaves
 ## usethis namespace: end
