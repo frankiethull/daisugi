f06c9bd6e52d24346480553bc2cc955ea8afae5a
frank hull <frankiethull@gmail.com>
2026-06-23 19:51:46 -0600

    update imports

--- R/daisugi-package.R
+++ R/daisugi-package.R
@@ -2,9 +2,14 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom catboost catboost.load_pool
+#' @importFrom catboost catboost.predict
+#' @importFrom catboost catboost.train
 #' @importFrom dials new_quant_param
 #' @importFrom evtree evtree
 #' @importFrom evtree evtree.control
+#' @importFrom lightgbm lgb.Dataset
+#' @importFrom lightgbm lgb.train
 #' @importFrom parsnip new_model_spec
 #' @importFrom parsnip set_encoding
 #' @importFrom parsnip set_fit
@@ -19,5 +24,7 @@
 #' @importFrom stats rnorm
 #' @importFrom tibble as_tibble
 #' @importFrom treeClust rpart.predict.leaves
+#' @importFrom xgboost xgb.DMatrix
+#' @importFrom xgboost xgb.train
 ## usethis namespace: end
 NULL
